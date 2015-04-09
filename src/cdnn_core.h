ce12cd19a6b626776386d0575be70fb0e20c3bc2
Da Li <daniel.lee.bit@gmail.com>
2015-04-08 23:49:49 -0700

    + Added "cdnn_core.h" to define internal data structure. + Added "Makefile" and compiled successfully.

---
+++ src/cdnn_core.h
@@ -0,0 +1,37 @@
+#ifndef __CDNN_CORE_H__
+#define __CDNN_CORE_H__
+
+struct cudnnContext {
+	
+
+
+
+};
+
+struct cudnnTensorStruct {
+	/* data */
+
+};
+
+struct cudnnConvolutionStruct {
+	/* data */
+
+};
+
+
+struct cudnnPoolingStruct {
+	/* data */
+
+};
+
+struct cudnnFilterStruct {
+	/* data */
+
+};
+
+struct cudnnFilterStruct {
+	/* data */
+
+};
+
+#endif
